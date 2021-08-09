5cd4db0647fba7478db359a9b204e9ce27d99eb5
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-08-09 15:41:00 -0400

    just in case

--- Lab5_1/Program.cs
+++ Lab5_1/Program.cs
@@ -2,6 +2,33 @@
 
 namespace Lab5_1
 {
+    class GameCharacter
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        //didn't make constructors yet.
+        public virtual void Play()
+        {
+            Console.WriteLine($"Name: {Name}.\nStrength: {Strength}\nIntelligence: {Intelligence}");
+        }
+    }
+
+    class MagicUsingCharacter : GameCharacter
+    {
+        public int MagicalEngergy;
+    }
+    class Warrior : GameCharacter
+    {
+        public string WeaponType;
+    }
+
+    class Wizard : MagicUsingCharacter
+    {
+        public int SpellNumber;
+    }
+
+
     class Program
     {
         static void Main(string[] args)
